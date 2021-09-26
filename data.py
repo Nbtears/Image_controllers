8652812a93facabddb66ffd11790d89fd53aec79
Diana Aguilar <dianaleticia2001@hotmail.com>
2021-09-25 21:55:06 -0500

    Gestion de base de datos desde python

--- data.py
+++ data.py
@@ -11,16 +11,42 @@ class DataBase():
         self.cursor = self.connection.cursor()
 
     def get_user(self):
-        sql = 'SELECT user FROM sesion ORDER BY date DESC LIMIT 1'.format()
+        sql = 'SELECT id,user FROM sesion ORDER BY date DESC LIMIT 1'.format()
         try :
             self.cursor.execute(sql)
             user = self.cursor.fetchone()
-
-            print (user)
+            print(user)
+            return user
 
         except:
             pass
 
+    def insert_data(self,id,rep):
+        sql = "INSERT INTO game (rep,sesion) VALUES (%s,%s)".format()
+        
+        try:
+            self.cursor.execute(sql,(rep,id))
+        except: 
+            pass
+
+    def print_data(self,name,id):
+        sql= "SELECT * FROM {} WHERE sesion = {}". format(name,id)
+        try:
+            self.cursor.execute(sql)
+            info = self.cursor.fetchone()
+            print(info)
+        except:
+            pass
+
+    def close(self):
+        self.connection.close()
+
 data = DataBase()
-data.get_user()
+user = data.get_user()
+data.insert_data(user[0],10)
+name = 'game'
+data.print_data(name,user[0])
+data.close()
+
+
 
